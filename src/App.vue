7daee32ffc276a507b7ec8c77fa4e36dac1d4df2
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-05-06 23:01:48 -0300

    added panel for searching by name

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
   <div id="app">
     <div class="cart">
-      <p>Carrinho: {{cart.length}}</p>
+      <p>Carrinho: {{ cart.length }}</p>
     </div>
     <product :premium="premium" @add-to-cart="updateCart"></product>
+    <Tela msg="Fiap Vue Pics" />
   </div>
 </template>
 
 <script>
 import Product from "./components/Product.vue";
+import Tela from "./components/Tela.vue";
 
 export default {
   name: "App",
   components: {
-    product: Product
+    product: Product,
+    Tela,
   },
   data() {
     return {
       premium: true,
-      cart: []
+      cart: [],
     };
   },
   methods: {
     updateCart(id) {
       this.cart.push(id);
-    }
-  }
+    },
+  },
 };
 </script>
 
